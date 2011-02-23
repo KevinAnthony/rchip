aed51c7e45acb104113f7ee1dfa9c10afca31d81
Kevin <kevin.s.anthony@gmail.com>
2011-02-22 23:46:33 -0500

    Changed the way malloc works, so large file names don't crash to program

--- src/notifications.c
+++ src/notifications.c
@@ -13,13 +13,13 @@
 
 gboolean setNotification(char* tickerString,char* notificationTitle,char* notificationText) {
 	#ifdef _SQL
-	char* query = (char *)malloc(1024);
-	char* msg = (char *)malloc(256);
-	sprintf(msg,"%s|%s|%s",tickerString,notificationTitle,notificationText);
+	char* msg = (char *)malloc(sizeof(tickerString)+sizeof(notificationTitle)+sizeof(notificationText)+5);
 	char* recipt = "7327943473";
-	//char*  recipt = (char *)malloc(64);
-	//recipt = get_registered_devices_message();
-	sprintf(query,"Insert into cmdQueue (command,cmdText,source_hostname,dest_hostname) values (\"%s\",\"%s\",\"%s\",\"%s\")","TMSG",msg,"Tomoya",recipt);
+	char* cmd = "TMSG";
+	char* hostname = "Tomoya";
+	char* query = (char *)malloc(sizeof(msg)+sizeof(recipt)+sizeof(cmd)+sizeof(hostname)+sizeof("Insert into cmdQueue (command,cmdText,source_hostname,dest_hostname) values (\"\",\"\",\"\",\"\")")+5);
+	sprintf(msg,"%s|%s|%s",tickerString,notificationTitle,notificationText);
+	sprintf(query,"Insert into cmdQueue (command,cmdText,source_hostname,dest_hostname) values (\"%s\",\"%s\",\"%s\",\"%s\")",cmd,msg,hostname,recipt);
 	sql_exec_quary(query);
 	return TRUE;
 	#else
