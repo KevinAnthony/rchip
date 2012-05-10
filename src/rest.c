92391e897dac9e8660098a665f43be504c63f69b
Kevin Anthony <kevin.s.anthony@gmail.com>
2012-05-10 18:18:27 -0400

    Updated rest.c to deal with the new response messages

--- src/rest.c
+++ src/rest.c
@@ -1,22 +1,22 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
-*
-*    rchip, Remote Controlled Home Integration Program
-*    Copyright (C) 2011 <Kevin@NoSideRacing.com>
-*
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ *
+ *    rchip, Remote Controlled Home Integration Program
+ *    Copyright (C) 2011 <Kevin@NoSideRacing.com>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <config.h>
 
@@ -64,22 +64,23 @@ void send_cmd_to_server(char* hostname,char* cmd,char* cmdTxt){
     }
 }
 size_t get_commands_callback(void *ptr,size_t size, size_t count, void* stream){
-    if (g_strcmp0(ptr,"\"Not Authorized\"") == 0){
-        return 0;
-    }
     json_object *jarray = json_tokener_parse((char*)ptr);
-    for (int i = 0; i< json_object_array_length(jarray); i++){
-        char *cmd;
-        char *cmd_txt;
-        json_object_object_foreach(json_object_array_get_idx(jarray, i), key, val){
-            if (g_strcmp0(key,"command") == 0){
-                cmd = json_object_get_string(val);
+    json_object *json_success = json_object_object_get(jarray, "success");
+    if (g_strcmp0(json_object_to_json_string(json_success),"true") == 0){
+        json_object *json_data = json_object_object_get(jarray, "data");
+        for (int i = 0; i< json_object_array_length(json_data); i++){
+            char *cmd;
+            char *cmd_txt;
+            json_object_object_foreach(json_object_array_get_idx(json_data, i), key, val){
+                if (g_strcmp0(key,"command") == 0){
+                    cmd = json_object_get_string(val);
+                }
+                if (g_strcmp0(key,"command_text") == 0){
+                    cmd_txt = json_object_get_string(val);
+                }
             }
-            if (g_strcmp0(key,"command_text") == 0){
-                cmd_txt = json_object_get_string(val);
-            }
+            process_cmd(cmd,cmd_txt);    
         }
-        process_cmd(cmd,cmd_txt);    
     }
     return 0;
 }
@@ -101,9 +102,13 @@ void get_active_devices( void ){
 
 size_t get_active_decives_callback(void *ptr,size_t size, size_t count, void* stream){
     json_object *jarray = json_tokener_parse((char*)ptr);
-    for (int i = 0; i< json_object_array_length(jarray); i++){
-        json_object_object_foreach(json_object_array_get_idx(jarray, i), key, val){
-            Hosts->add(json_object_get_string(val));
+    json_object *json_success = json_object_object_get(jarray, "success");
+    if (g_strcmp0(json_object_to_json_string(json_success),"true") == 0){
+        json_object *json_data = json_object_object_get(jarray, "data");
+        for (int i = 0; i< json_object_array_length(json_data); i++){
+            json_object_object_foreach(json_object_array_get_idx(json_data, i), key, val){
+                Hosts->add(json_object_get_string(val));
+            }
         }
     }
     return 0;
@@ -139,7 +144,6 @@ void authenticate( void ){
     char* username = get_setting_str(REST_USERNAME);
     char* password = get_setting_str(REST_PASSWORD);
     char* url = g_strconcat(URL,"/authenticate/?username=",username,"&password=",password,NULL);
-    char* response;
     session = curl_easy_init();
     if (session) {
         curl_easy_setopt(session, CURLOPT_URL, url);
